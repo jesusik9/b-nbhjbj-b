69e17dedf55d959ad067d16dfd17e47704a680be
jesusik9 <121265760+jesusik9@users.noreply.github.com>
2023-06-20 15:55:56 -0600

    Add files via upload

---
+++ holas.js
@@ -0,0 +1,18 @@
+song="";
+function preload(){
+song=loadSound("musica.mp3")
+
+
+}
+rightWristX=0;
+leftWristY=0;
+rightWristY=0;
+leftWristX=0;
+
+scoreRightWrist=0;
+scoreLeftWrist=0;
+function setup(){
+
+    canvas=createCanvas(1000,500);
+    canvas.parent
+}
